e8de664b8b7276cf315f2a0e60349ff2d7b8810e
PrudhviRaju06 <168115596+PrudhviRaju06@users.noreply.github.com>
2024-04-26 16:43:49 +0530

    files upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CarAB.MvcApplication" Language="C#" %>
---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+SELECT TOP (1000) [cat_id]
+      ,[cat_name]
+      ,[cat_image]
+      ,[cat_ad]
+      ,[cat_stat]
+  FROM [dbcar].[dbo].[tbl_category]
